57f8185783e0ab8df4b3f7e039fb89412d66e086
Azima05 <bekbooevaazima@gmail.com>
2024-11-28 14:45:59 +0300

    Update mat.py

--- mat.py
+++ mat.py
@@ -1,4 +1,10 @@
 import random
+import os
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+BOARD_SIZE = 7
+    print("  A B C D E F G")
+    print("  ---------------")
 def play_game()
   player_name = input("Enter your name: ")
   print("Welcome to Battleship!")
