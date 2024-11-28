4d8e4106a37be03537d204e731da0b38cf5d4d1d
Azima05 <bekbooevaazima@gmail.com>
2024-11-28 20:48:23 +0300

    Update mat.py

--- mat.py
+++ mat.py
@@ -4,6 +4,9 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 BOARD_SIZE = 7
 SHIP_SIZES = [3, 2, 2, 1, 1, 1, 1]
+EMPTY = " " 
+HIT = "X"  
+MISS = "O" 
     print("  A B C D E F G")
     print("  ---------------")
 def play_game()
