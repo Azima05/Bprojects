f08c6bbfdd38304b66e33c124ba3ac57e489f9a0
Azima05 <bekbooevaazima@gmail.com>
2024-12-02 12:58:42 +0300

    Update mat.py

--- mat.py
+++ mat.py
@@ -19,7 +19,17 @@ for ship in ships:
     for coord in ship:
         ship_positions.append(coord)
 shots = 0
-    print("  A B C D E F G")
-    print("  ---------------")
-    
 
+while ship_positions: 
+    clear_screen()
+    print("   A B C D E F G\n  ---------------")
+    for row in range(7):
+        print(f"{row + 1} | {' '.join(board[row])}")
+    print(f"Shots: {shots}")
+ shot = input("Please choose and enter coordinates (e.g., A1): ").strip().upper()
+
+if len(shot) != 2 or shot[0] not in 'ABCDEFG' or not shot[1].isdigit():
+        print("Incorrect format. Try again please!")
+        input("Click Enter to continue...")
+        continue
+col, row = 'ABCDEFG'.index(shot[0]), int(shot[1]) - 1
