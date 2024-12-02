cfec968d46162bb28fb255604992913778a00dd9
Azima05 <bekbooevaazima@gmail.com>
2024-12-02 12:52:17 +0300

    Update mat.py

--- mat.py
+++ mat.py
@@ -1,23 +1,25 @@
 import random
 import os
+
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
-BOARD_SIZE = 7
-SHIP_SIZES = [3, 2, 2, 1, 1, 1, 1]
-EMPTY = " " 
-HIT = "X"  
-MISS = "O" 
+clear_screen():
+print("Welcome to Battleship!")
+
+ships = [
+    [(2, 2), (2, 3), (2, 4)], 
+    [(4, 5), (5, 5)],  
+    [(1, 6)],
+    [(6, 0)]  
+]
+board = [['♥' for _ in range(7)] for _ in range(7)]
+
+ship_positions = []
+for ship in ships:
+    for coord in ship:
+        ship_positions.append(coord)
+shots = 0
     print("  A B C D E F G")
     print("  ---------------")
-def display_grid():
-    print("  A B C D E F G")
-    for i in range(7):
-        print(f"{i+1} ", end="")
-        print(" ".join([" "]*7))
-display_grid():
-def play_game()
-  player_name = input("Enter your name: ")
-  print("Welcome to Battleship!")
+    
 
-
-play_game()
