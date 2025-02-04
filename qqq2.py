7d294bee20924f29c7503c453b22cbe2a5775586
Azima05 <bekbooevaazima@gmail.com>
2025-02-04 12:41:03 +0300

    Create qqq2.py

---
+++ qqq2.py
@@ -0,0 +1,9 @@
+laptop = { "brand": "dell", "model": "alienware", "year": 2010 }
+
+print("Brand of the laptop:", laptop["brand"])
+
+laptop["home"] = True
+print("Updated laptop information:", laptop)
+
+laptop["year"] = 2019
+print("Updated year of the laptop:", laptop)
