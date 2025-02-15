d1dcfc8232abecf12e4d123d6c6daf67a4186e8e
Azima05 <bekbooevaazima@gmail.com>
2025-02-15 13:33:16 +0300

    Update dt..py

--- dt..py
+++ dt..py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
-
+#2
 months_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 months_index = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 month_series = pd.Series(months_data, index=months_index)
 print("Months Series:")
 print(month_series)
 print()
-
+#3
 batch_data = {'MatMIE': 45, 'Mat DAIS': 30, 'COMIE': 50, 'COMEC': 40}
 batch_series = pd.Series(batch_data)
 print("Batch Groups Series:")
 print(batch_series)
 print()
 
-
+#4
 exam_data = {
     'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
     'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
@@ -27,7 +27,7 @@ print("Exam DataFrame:")
 print(df)
 print()
 
-
+#5
 attempts_greater_than_2 = df[df['attempts'] > 2]
 print("Number of attempts in the examination is greater than 2:")
 print(attempts_greater_than_2)
