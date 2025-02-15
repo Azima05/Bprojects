3b29e6b284a7eff9f91cd71889c29bb34607ff07
Azima05 <bekbooevaazima@gmail.com>
2025-02-15 14:38:14 +0300

    Update dt..py

--- dt..py
+++ dt..py
@@ -16,7 +16,7 @@ print()
 
 #4
 exam_data = {
-    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'name': ['Aigerim', 'Angela', 'Aizhan', 'Meerim', 'Aruuke', 'Azima', 'Aidai', 'Aijan', 'Azem', 'Aidana'],
     'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
     'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
     'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
